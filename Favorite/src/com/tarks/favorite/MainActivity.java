9a97c5cab6680d38a759c2de6d65c80ff2537fda
JHRunning <jeonghunn1@gmail.com>
2014-03-11 01:26:46 +0900

    Beta Build 1516 : MainActivity permission alert bug fixed

--- Favorite/src/com/tarks/favorite/MainActivity.java
+++ Favorite/src/com/tarks/favorite/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends SherlockActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
-
+	
 		// 자신의 신분 설정값을 불러옵니다.
 		SharedPreferences prefs = getSharedPreferences("setting", MODE_PRIVATE);
 		String frist_use = prefs.getString("frist_use_app", "true");
@@ -209,7 +209,6 @@ public class MainActivity extends SherlockActivity {
 				});
 		builder.show();
 
-		builder.show();
 	}
 
 	public void IPError() {
